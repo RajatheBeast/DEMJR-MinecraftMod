122a4fa31fc17cce5e80be86a17809a8ae322f72
Elliott Starchuk <pixelkips@outlook.com>
2016-12-14 16:56:48 -0800

    Shut up Git

--- src/main/java/demjr/mod/blocks/BlockSalt.java
+++ src/main/java/demjr/mod/blocks/BlockSalt.java
@@ -11,6 +11,7 @@ public class BlockSalt extends Block {
 
 		setUnlocalizedName(Reference.ModBlocks.SALT.getUnlocalizedName());
 		setRegistryName(Reference.ModBlocks.SALT.getRegistryName());
+
 	
 	}
 
