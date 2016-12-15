e05361faa4f3b070c2244cde41b0c8ca307b3f9d
Elliott Starchuk <pixelkips@outlook.com>
2016-12-14 17:28:03 -0800

    Made salt drop

--- src/main/java/demjr/mod/blocks/BlockSalt.java
+++ src/main/java/demjr/mod/blocks/BlockSalt.java
@@ -1,18 +1,38 @@
 package demjr.mod.blocks;
 
 import demjr.mod.Reference;
+import demjr.mod.init.ModBlocks;
+import demjr.mod.init.ModItems;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.Item;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+import javax.annotation.Nullable;
+import java.util.Random;
 
 public class BlockSalt extends Block {
 
 	public BlockSalt() {
-		super(Material.ROCK);
+        super(Material.ROCK);
 
-		setUnlocalizedName(Reference.ModBlocks.SALT.getUnlocalizedName());
-		setRegistryName(Reference.ModBlocks.SALT.getRegistryName());
+        setUnlocalizedName(Reference.ModBlocks.SALT.getUnlocalizedName());
+        setRegistryName(Reference.ModBlocks.SALT.getRegistryName());
 
-	
-	}
+    }
 
+    @Nullable
+    public Item getItemDropped(IBlockState state, Random rand, int fortune){
+        return this == ModBlocks.salt ? ModItems.salt :  Item.getItemFromBlock(this);
+    }
+
+    public int quantityDropped(Random random){
+        return this == ModBlocks.salt ? 2 + random.nextInt(5) : 1;
+    }
+
+    public void dropBlockAsItemWithChance(World worldIn, BlockPos pos, IBlockState state, float chance, int fortune){
+        super.dropBlockAsItemWithChance(worldIn, pos, state, chance, fortune);
+    }
 }
