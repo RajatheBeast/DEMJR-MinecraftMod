3dca50046a76e6d3ef9037b9bdbe7a71dec0660b
pixelkips <pixelkips@outlook.com>
2016-12-18 16:56:41 -0800

    Re-wrote for fixes

--- src/main/java/demjr/mod/items/ItemTrumpGem.java
+++ src/main/java/demjr/mod/items/ItemTrumpGem.java
@@ -3,13 +3,13 @@ package demjr.mod.items;
 import demjr.mod.Reference;
 import net.minecraft.item.Item;
 
-/**
- * Created by Elliott on 2016-12-15.
- */
 public class ItemTrumpGem extends Item {
+	
+	public ItemTrumpGem() {
+		
+		setUnlocalizedName(Reference.ModItems.TRUMPGEM.getUnlocalizedName());
+		setRegistryName(Reference.ModItems.TRUMPGEM.getRegistryName());
+		
+	}
 
-    public ItemTrumpGem() {
-        setUnlocalizedName(Reference.ModItems.TRUMPGEM.getUnlocalizedName());
-        setRegistryName(Reference.ModItems.TRUMPGEM.getRegistryName());
-    }
 }
