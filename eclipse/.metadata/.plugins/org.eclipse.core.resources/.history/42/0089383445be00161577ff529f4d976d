b44ddf76f369153c028a4a09d5ade079ec97848d
D-Daws <ddog061@gmail.com>
2016-12-20 21:11:40 -0500

    LOL!

---
+++ eclipse/.metadata/.plugins/org.eclipse.core.resources/.history/42/0089383445be00161577ff529f4d976d
@@ -0,0 +1,35 @@
+package demjr.mod.init;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+public class ModBlocks {
+	
+	public static void init() {
+		
+	}
+	public static void register() {
+	
+	}
+		
+	public static void registerRenders() {
+	
+	}
+	
+	private static void registerBlock(Block block) {
+		GameRegistry.register(block);
+		ItemBlock item = new ItemBlock(block);
+		item.setRegistryName(block.getRegistryName());
+		GameRegistry.register(item);
+		
+	}
+		
+	private static void registerRender(Block block) {
+		Minecraft.getMinecraft().getRenderItem().getItemModelMesher().register(Item.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName(), "inventory"));
+	}
+
+}
---
+++ src/main/java/demjr/mod/Reference.java
@@ -0,0 +1,60 @@
+package demjr.mod;
+
+public class Reference {
+		
+	public static final String MOD_ID = "demjr";
+	public static final String NAME = "DEMJR";
+	public static final String VERSION = "1.0";
+	public static final String ACCEPTED_VERSIONS = "[1.9.4]";
+		
+	public static final String CLIENT_PROXY_CLASS = "demjr.mod.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "demjr.mod.proxy.ServerProxy";
+	
+	public static enum ModItems {
+		
+		SALT("salt", "ItemSalt"),
+        FLOUR("flour", "ItemFlour"),
+        TRUMPGEM("trumpgem", "ItemTrumpGem");
+		
+		private String unlocalizedName;
+		private String registryName;		
+		
+		ModItems(String unlocalizedName, String registryName) {
+			this.unlocalizedName = unlocalizedName;
+			this.registryName = registryName;
+		}
+		public String getUnlocalizedName() {
+			return unlocalizedName;
+		}
+		
+		public String getRegistryName() {
+			return registryName;
+		}
+		
+	}
+	
+	public static enum ModBlocks {
+
+		SALT("salt", "BlockSalt"),
+        TRUMPIUMORE("trumpiumore", "BlockTrumpiumOre");
+		
+		private String unlocalizedName;
+		private String registryName;		
+		
+		ModBlocks(String unlocalizedName, String registryName) {
+			this.unlocalizedName = unlocalizedName;
+			this.registryName = registryName;
+		}
+		public String getUnlocalizedName() {
+			return unlocalizedName;
+		}
+		
+		public String getRegistryName() {
+			return registryName;
+		}
+		
+	}
+
+}
+
+
