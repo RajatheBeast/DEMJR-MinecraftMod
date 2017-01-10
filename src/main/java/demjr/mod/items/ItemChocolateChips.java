e31fde942d0be0a0d94d06b3baa1ea477bc11cdf
Kona01 <matthewjacobs@hotmail.ca>
2017-01-10 14:58:57 -0800

    no

--- src/main/java/demjr/mod/items/ItemChocolateChips.java
+++ src/main/java/demjr/mod/items/ItemChocolateChips.java
@@ -1,20 +1,7 @@
 package demjr.mod.items;
 
 import demjr.mod.Reference;
-<<<<<<< HEAD
 
-import net.minecraft.item.Item;
-
-/**
- * Created by Elliott on 2017-01-09.
- */
-public class ItemChocolateChips extends Item {
-
-    public ItemChocolateChips() {
-        setUnlocalizedName(Reference.ModItems.CHOCOLATECHIPS.getUnlocalizedName());
-        setRegistryName(Reference.ModItems.CHOCOLATECHIPS.getRegistryName());
-    }
-=======
 import net.minecraft.item.Item;
 
 public class ItemChocolateChips extends Item {
@@ -25,6 +12,5 @@ public class ItemChocolateChips extends Item {
 		setRegistryName(Reference.ModItems.CHOCOLATECHIPS.getRegistryName());
 		
 	}
->>>>>>> 76fd6187332ae1d2480ffe09106961cd61e0b30d
 
 }
