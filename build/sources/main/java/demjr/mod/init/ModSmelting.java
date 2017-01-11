5fe4a90bae121f22ad39f9b1f54ad9674ef3ef8a
Elliott Starchuk <pixelkips@outlook.com>
2017-01-11 11:40:34 -0800

    Smelting

---
+++ build/sources/main/java/demjr/mod/init/ModSmelting.java
@@ -0,0 +1,16 @@
+package demjr.mod.init;
+
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.common.registry.GameRegistry;
+
+
+/**
+ * Created by Elliott on 2017-01-11.
+ */
+public class ModSmelting {
+    public static void register() {
+        GameRegistry.addSmelting(ModItems.dough, new ItemStack(Items.BREAD), 6);
+    }
+
+}
